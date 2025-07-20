401b336a34666e4f50fe1a7d3d8cde287716a22c
Andrezinrc <andrem.33333@gmail.com>
2025-07-20 06:15:09 +0000

    feat: adicionar botão menu hambúrguer

--- Header.js
+++ Header.js
@@ -3,11 +3,20 @@ import Menu from './components/Menu';
 
 function Header() {
   return (
-    <header className="cabecalho-portfolio">
-      <div className="title">
-        <h1>Andrecode</h1>
+    <header className="header">
+      <div className="header-content">
+        <div className="title">
+          <h1>Andrecode</h1>
+        </div>
+        
+        <div className="hamburguer">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
       </div>
-      {/* Menu de navegação principal */}
+      
+      { /* Menu de navegação principal */ }
       <Menu />
     </header>
   );
--- components/Menu.js
+++ components/Menu.js
@@ -3,12 +3,22 @@ import React from 'react';
 function Menu() {
   return (
     <nav className="nav-bar">
+      {/* Menu Desktop */}
       <ul className="menu-desk">
-        <li className="nav-list">Inicio</li>
-        <li className="nav-list">Sobre mim</li>
-        <li className="nav-list">Habilidades</li>
-        <li className="nav-list">Projetos</li>
-        <li className="nav-list">Contato</li>
+        <li className="nav-list"><a className="nav-link">Inicio</a></li>
+        <li className="nav-list"><a className="nav-link">Sobre mim</a></li>
+        <li className="nav-list"><a className="nav-link">Habilidades</a></li>
+        <li className="nav-list"><a className="nav-link">Projetos</a></li>
+        <li className="nav-list"><a className="nav-link">Contato</a></li>
+      </ul>
+      
+      {/* Menu Mobile */}
+      <ul className="menu-mobile">
+        <li className="nav-list"><a className="nav-link">Inicio</a></li>
+        <li className="nav-list"><a className="nav-link">Sobre mim</a></li>
+        <li className="nav-list"><a className="nav-link">Habilidades</a></li>
+        <li className="nav-list"><a className="nav-link">Projetos</a></li>
+        <li className="nav-list"><a className="nav-link">Contato</a></li>
       </ul>
     </nav>
   );
