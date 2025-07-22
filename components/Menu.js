4fd191f36230a55bd0fa2eedec0c1905d04ea874
Andrezinrc <andrem.33333@gmail.com>
2025-07-22 07:13:03 +0000

    fix: fechar menu ao clicar em links

--- components/Menu.js
+++ components/Menu.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
 
 function Menu() {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setIsMenuOpen] = useState(false);
 
   // Função para alternar o estado do menu
   const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+    setIsMenuOpen(!menuOpen);
   }
   
+  function handleLinkClick() {
+    setIsMenuOpen(false); // Fecha o menu ao clicar no link
+  }
+
+
   return (
     <nav className="nav-bar">
       {/* Menu Hamburguer */}
@@ -32,18 +37,19 @@ function Menu() {
         <li className="nav-list"><a href="#contact" className="nav-link">Contato</a></li>
       </ul>
       
-      {menuOpen && <div className="overlay" onClick={() => setMenuOpen(false)}></div>}
+      {menuOpen && <div className="overlay" onClick={() => setIsMenuOpen(false)}></div>}
       
       {/* Menu Mobile */}
       <ul className={`menu-mobile ${menuOpen ? 'open' : ''}`}>
-        <li className="nav-list"><a href="#" className="nav-link">Inicio</a></li>
-        <li className="nav-list"><a href="#about" className="nav-link">Sobre mim</a></li>
-        <li className="nav-list"><a href="#skills" className="nav-link">Habilidades</a></li>
-        <li className="nav-list"><a href="#certificates" className="nav-link">Certificados</a></li>
-        <li className="nav-list"><a href="#projects" className="nav-link">Projetos</a></li>
-        <li className="nav-list"><a href="#contact" className="nav-link">Contato</a></li>
+        <li className="nav-list"><a href="#" className="nav-link" onClick={handleLinkClick}>Inicio</a></li>
+        <li className="nav-list"><a href="#about" className="nav-link" onClick={handleLinkClick}>Sobre mim</a></li>
+        <li className="nav-list"><a href="#skills" className="nav-link" onClick={handleLinkClick}>Habilidades</a></li>
+        <li className="nav-list"><a href="#certificates" className="nav-link" onClick={handleLinkClick}>Certificados</a></li>
+        <li className="nav-list"><a href="#projects" className="nav-link" onClick={handleLinkClick}>Projetos</a></li>
+        <li className="nav-list"><a href="#contact" className="nav-link" onClick={handleLinkClick}>Contato</a></li>
       </ul>
     </nav>
   );
 };
 export default Menu;
+
