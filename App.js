f759f78686712b36df2a2c171dc0799fd4902e10
Andrezinrc <andrem.33333@gmail.com>
2025-07-23 02:07:07 +0000

    feat: adiciona tela de carregamento inicial com spinner

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './Header';
 import Content from './Content.js';
 import Footer from './Footer.js';
 
 function App() {
+  useEffect(() => {
+    const loadingScreen = document.getElementById('loading-screen');
+    if (loadingScreen) {
+      loadingScreen.style.display = 'none';
+    }
+  }, []);
+  
   return (
     <div>
-      { /* Cabeçalho do Portfólio */ }
-      <Header />
-      
-      { /* Conteúdo Principal do Portfólio */ }
-      <Content />
-      
-      {/* Rodapé do Portfólio */}
-      <Footer />
-    </div>
+        {/* Cabeçalho do Portfólio */}
+        <Header />
+        
+        {/* Conteúdo Principal do Portfólio */}
+        <Content />
+        
+        {/* Rodapé do Portfólio */}
+        <Footer />
+      </div>
   );
 }
+
 export default App;
